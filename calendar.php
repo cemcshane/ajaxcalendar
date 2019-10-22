d5ca04310f18e9d0bab0a186487186c7f8b73758
Claire McShane <cemcshane@wustl.edu>
2019-10-22 03:19:37 -0500

    working

--- calendar.php
+++ calendar.php
@@ -191,25 +191,15 @@
             }  
             let countDownDate = new Date(year, month, day, hour, min);
 
-            // Update the count down every 1 second
             sessionStorage.setItem("x", setInterval(function() {
-                // Get today's date and time
                 let now = new Date().getTime();
-
-                // Find the distance between now and the count down date
                 let distance = countDownDate - now;
-
-                // Time calculations for days, hours, minutes and seconds
                 let days = Math.floor(distance / (1000 * 60 * 60 * 24));
                 let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
                 let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
                 let seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-                // Display the result in the element with id="demo"
                 document.getElementById("timer").innerHTML = days + "d " + hours + "h "
                 + minutes + "m " + seconds + "s ";
-
-                // If the count down is finished, write some text
                 if (distance < 0) {
                     clearInterval(sessionStorage.getItem("x"));
                     document.getElementById("timer").innerHTML = "EXPIRED: This date has already passed.";
